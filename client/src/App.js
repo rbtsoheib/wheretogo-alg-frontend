59e00c4ef9426ccdf6ad520db41b43b4a8359e07
rbtsoheib <taharsoheib08@gmail.com>
2024-10-18 00:15:03 +0200

    i made a small header, no css, i will find assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
+import React from "react";
+import { Link } from "react-dom"
+import "./App.css";
 
 function App() {
   return (
-<>
-
-
-</>
-);
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li><Link path=""></Link></li>
+            <li><Link path=""></Link></li>
+            <li><Link path=""></Link></li>
+            <li><Link path=""></Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default App;
